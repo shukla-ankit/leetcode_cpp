13abb381b90702aa8ee92469720e267c5d18abfb
Ankit Shukla <shukla.ankit@protonmail.com>
2021-03-09 08:15:09 -0500

    problem 5 : not done

--- source/5_LongestPalindromicSubstring.cpp
+++ source/5_LongestPalindromicSubstring.cpp
@@ -9,11 +9,17 @@ using namespace std;
 
 class Solution {
     public:
-        
+    int lengthWhenPalindromeCenter(string s, int index){
+
+    }
+    string longestPalindrome(string s) {
+        int lenPosLongestPal = s.length();
+    }
 };
 
 int main() {
     Solution sol;
+    vector<string> vecTests = {"babad", "cbbd", "a", "ac"};
 
 
 	return 0;
