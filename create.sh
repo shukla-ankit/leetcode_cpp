c9a5a2e54b93d5d9b055ceab1ac430a9420a3118
Ankit Shukla <shukla.ankit@protonmail.com>
2021-03-10 06:42:49 -0500

    rename files to order ascendingly

--- create.sh
+++ create.sh
@@ -1,6 +1,21 @@
 num=`echo $1 | cut -d '.' -f1`
 name=`echo $1 | cut -d '.' -f2`
-cppFileName=`echo $num"_"${name// /}".cpp"`
+zeroes=""
+if [ "${#num}" == "1" ] 
+then
+    zeroes="000"
+elif [ "${#num}" == "2" ]
+then
+    zeroes="00"
+elif [ "${#num}" == "3" ]
+then
+    zeroes="0"
+elif [ "${#num}" == "4" ]
+then
+    zeroes=""
+fi
+cppFileName=$zeroes`echo $num"_"${name// /}".cpp"`
+
 echo $cppFileName" created!"
 echo "//LeetCode Problem "$num" -"$name"
 
