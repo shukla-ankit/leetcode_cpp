ca02e5366ce9a3bd1ac52754bc76111e304fc110
Ankit Shukla <shukla.ankit@protonmail.com>
2021-03-11 04:48:50 -0500

    Problem 9

--- source/09_PalindromeNumber.cpp
+++ source/09_PalindromeNumber.cpp
@@ -15,8 +15,20 @@ Color::Modifier def(Color::FG_DEFAULT);
 
 class Solution {
     public:
+    string reverseStr(string s){
+        string ret;            
+        for(int i = s.length() -1; i >-1; i--){
+            ret += s[i];
+        }
+        return ret;
+    }
+
     bool isPalindrome(int x) {
+        if( x < 0)
+            return false;   
         
+        string s = to_string(x);
+        return s == reverseStr(s);
     }        
 };
 
@@ -28,15 +40,16 @@ struct Test{
 int main() {
     Solution sol;
     vector<Test> vecTests = {
-        {.x = 123, .sol = true},
+        {.x = 123, .sol = false},
         {.x = -121, .sol = false},
         {.x = -10, .sol = false},
         {.x = -101, .sol = false},
+        {.x = 101, .sol = true},
     };
     int count = 0;
     for(auto test: vecTests){        
-        int ret = sol.isPalindrome(test.x);
-        cout << "Test #" << ++count << " : Input = " << test.x << ", Output = " << ret << ". Result : " <<  (ret == test.sol? green : red) << (ret == test.sol? "Pass" : "Fail") << "!" << def << endl;
+        bool ret = sol.isPalindrome(test.x);
+        cout << "Test #" << ++count << " : Input = " << test.x << ", Output = " << ret << ". Result : " <<  (ret == test.sol? green : red) << (ret == test.sol? "Pass!" : "Fail!") <<  def << endl;
     }
 	return 0;
 }
