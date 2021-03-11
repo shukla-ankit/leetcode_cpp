8207b1d5909a24d7981ef6a3e4ceb0326df9e92a
Ankit Shukla <shukla.ankit@protonmail.com>
2021-03-11 01:25:06 -0500

    P6

--- source/0006_ZigZagConversion.cpp
+++ source/0006_ZigZagConversion.cpp
@@ -10,15 +10,22 @@ using namespace std;
 class Solution {
     public:
         string convert(string s, int numRows) {
-            string converted;
-            for(int i=0; i< s.length(); i+= 2*(numRows -1)){
+            if(numRows == 1) return s;
+            string converted;            
+            for (int i=0; i< s.length(); i+= 2*(numRows -1)){
                 converted += s[i];
             }
-            for(int i=1; i < numRows-1; i++){
-                for(int j=0; j < s.length(); j += 2*(numRows -1)){
-                    converted += s[2*(numRows -1) + i];
-                    converted += s[2*(numRows -1) - i];
-                 }            
+            for (int i = 1; i < numRows-1 && numRows > 2; i++){
+                for (int k = 0; k < s.length()/2*(numRows-1); k++){
+                    if(s.length() > 2*(numRows-1)* k   + i) 
+                        converted += s[ 2*(numRows-1)*k + i ];
+                    else
+                        break;
+                    if(s.length() > 2*(numRows-1)*(k+1)- i) 
+                        converted += s[ 2*(numRows-1)*(k+1) - i ];
+                    else
+                        break;
+                }
             }
             for(int i = numRows-1; i < s.length(); i+= 2*(numRows -1)){
                 converted += s[i];
@@ -26,6 +33,7 @@ class Solution {
             return converted;
         }
 };
+
 struct Test{
     string str;
     int rows;
@@ -35,7 +43,11 @@ int main() {
     Solution sol;
     vector<Test> vecTests = {{"PAYPALISHIRING", 3, "PAHNAPLSIIGYIR"}, 
                              {"PAYPALISHIRING", 4, "PINALSIGYAHRPI"},
-                             {"A", 1, "A"}};
+                             {"A", 1, "A"},
+                             {"maptvhyyewsggiuasuakgzumqwotffqrhglcpldthvzpdwpvqpizqclgabbfgrznxmrnzuigpkxvgosyfaxxeidflgmrzngzzymyswgkgdfotxnyakvevalgiyailghngvnbtulazsqvpftrqrwnrhtahvkvcrkkoxlhtyjvsaqifjbxaxkuhgwqbglfzvqnvduoeejwgzgnlinnhzhofffhlsokqgxlkuzqalmimvxxdknkkwbrcganapaqvzbhtdxvomdahdamnnwzjzrlhtbiidygccnyfntvbzviexurkstwsmjzfkjqniwsmlqralmbmjlqjfkvadrurbvwnfobpmvbyluawicltnbcvnyxsprjsmigtwjijeljrflpnnahdelarjxkbqttebbyakijquuhbfxrvxyabjavvzfwrarvctvedenwajdboaulasldenybmfxdgobkjwopcdlcmogcraotvzybnxcbebfkrgubeiqhldlzttckwqfrpeuedwghxnsovorzzhimkumepoqlgwevcycfwiovxgksxdtwlcixyudnkuzqsdoweqbaapyxykrxnktymdykabykxzbrernkhqnjiliivzfijyjwdhidkiokhrboipyrhlapwixrhccscloguzjehzorqsfahdrortgnddhkijfkuvsoucucblvudaumfm", 549, 
+                             "maptvhyyewsggiuasuakgzumqwotffqrhglcpldthvzpdwpvqpizqclgabbfgrznxmrnzuigpkxvgosyfaxxeidflgmrzngzzymyswgkgdfotxnyakvevalgiyailghngvnbtulazsqvpftrqrwnrhtahvkvcrkkoxlhtyjvsaqifjbxaxkuhgwqbglfzvqnvduoeejwgzgnlinnhzhofffhlsokqgxlkuzqalmimvxxdknkkwbrcganapaqvzbhtdxvomdahdamnnwzjzrlhtbiidygccnyfntvbzviexurkstwsmjzfkjqniwsmlqralmbmjlqjfkvadrurbvwnfobpmvbyluawicltnbcvnyxsprjsmigtwjijeljrflpnnahdelarjxkwmibffoqcmvtyuxtcagevdkbeusbwvxygldalbtkqcwiouljpccqeuiumoxuusyhkvubmudfiknxhfkrzjuvzizxrkqyohsavddbodojsnwangevxtqvhrbzgoafwrawddprehyauaxrefyvpskcrqrtfrxvqonewzkdkhteceyntjmwtzdazuyjlgkddoabllbohcyaqskuicxleczabhbsurrlgxedrirekwnnfpkybahbelqmbhnfcrjxxyidnplgbiioyoibzbvkvrzjthfwokioaojpriycckjdgdwloidcmh"}
+                             };
+
     for(auto test: vecTests){
         string res =  sol.convert(test.str,test.rows);
         cout << res << ", matched = " << (res == test.solution? "YES" : "NO") << endl;
