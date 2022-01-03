f5c6f7f9233859d22c7d7471da3f8c2d0ed64f20
Ankit Shukla <ankitshukla34@gmail.com>
2022-01-03 18:41:08 -0500

    clean up

---
+++ src/0445_AddTwoNumbersII.cpp
@@ -0,0 +1,32 @@
+//LeetCode Problem 445 - Add Two Numbers II
+#include "everything.h"
+
+struct Test{
+};
+class Solution {
+    public:
+        method(){
+        }
+        leetcode_solution(){
+        }
+};
+int main() {
+    Solution sol;
+    vector<Test> vecTests = {};
+    int count = 0;
+    for(auto test: vecTests){        
+        int output = sol.XXXX(test.input);
+        bool bIsPass = output == test.output;
+        cout << "Test #" << ++count << " : Input = " << test.input << ", Output = " << output << ". Result : " <<  (bIsPass? green : red) << (bIsPass? "Pass" : "Fail") << "!" << def << endl;
+    }
+	return 0;
+}
+/*
+-----------------------------------------------------------------------------------------------------------
+method1: <<>>
+-----------------------------------------------------------------------------------------------------------
+Leetcode Method:
+
+-----------------------------------------------------------------------------------------------------------
+*/
+
