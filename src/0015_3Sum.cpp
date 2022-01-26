e0ed146c51cfc0a30cc6a0129270ab674d3def37
Ankit Shukla <ankitshukla34@gmail.com>
2022-01-26 07:58:49 -0500

    15. Three Sum

--- src/0015_3Sum.cpp
+++ src/0015_3Sum.cpp
@@ -1,6 +1,14 @@
 //LeetCode Problem 15 - 3Sum
 #include "everything.h"
 
+/*
+ * The bruteforce way will be to do three loops and check for each value with O(n^3)
+ * Or we do two loops and find the negative of their sum using binary search with O(n^2 log n)
+ * Or we do one loop and try to find two numbers with sum equal to negative as first,
+ * which gives O(n^2)
+ * */
+
+
 struct Test{
     vector<int> input;
     vector<vector<int>> output;
@@ -13,14 +21,15 @@ class Solution {
         vector<vector<int>> result;
         sort(nums.begin(), nums.end());
         for (int i = 0; i < n - 2; i++) {
-            if (i == 0 || nums[i] != nums[i - 1]) {
+            if (i == 0 || nums[i] != nums[i - 1]) // To avoid repeatition of i
+            {
                 int j = i + 1, k = n - 1;
                 while (j < k) {
                     int sum = nums[i] + nums[j] + nums[k];
                     if (sum == 0) {
                         result.push_back({nums[i], nums[j], nums[k]});
-                        while (j < k && nums[j] == nums[j + 1]) j++;
-                        while (j < k && nums[k] == nums[j - 1]) k--;
+                        while (j < k && nums[j] == nums[j + 1]) j++; //Avoid repeatition in j
+                        while (j < k && nums[k] == nums[j - 1]) k--; //Avoid repeatition in k
                         j++;
                         k--;
                     }
