fcce0a22e3478e07ee7ae762553f71da7c8fc97c
Ankit Shukla <ankitshukla34@gmail.com>
2022-01-27 06:34:23 -0500

    0017 Letter Combinations of a Phone Number

---
+++ src/0017_LetterCombinationsofaPhoneNumber.cpp
@@ -0,0 +1,74 @@
+//LeetCode Problem 17 - Letter Combinations of a Phone Number
+#include "everything.h"
+
+struct Test{
+    string input;
+    vector<string> output;
+};
+class Solution {
+public:
+    vector<string> addChar(vector<string> s, vector<char> digitSymbols){
+        vector<string> res;
+        if(s.empty()){
+            for(int j = 0; j < digitSymbols.size(); j++){
+                string t;
+                t.push_back(digitSymbols[j]);
+                res.push_back(t);
+            }
+            return res;
+        }
+
+        for(int i = 0; i < s.size(); i++){
+            for(int j = 0; j < digitSymbols.size(); j++){
+                string t;
+                t.push_back(digitSymbols[j]);
+                res.push_back(s[i] + t);
+            }
+        }
+        return res;
+    }
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        map<char,vector<char>> m = {
+                {'1', {}},
+                {'2', {'a','b','c'}},
+                {'3', {'d','e','f'}},
+                {'4', {'g','h','i'}},
+                {'5', {'j','k','l'}},
+                {'6', {'m','n','o'}},
+                {'7', {'p','q','r','s'}},
+                {'8', {'t','u','v'}},
+                {'9', {'w','x','y','z'}},
+                {'0', {' '}}
+        };
+        for(int i = 0; i < digits.length(); i++){
+            result = addChar(result, m[digits[i]]);
+        }
+        return result;
+    }
+};
+int main() {
+    Solution sol;
+    vector<Test> vecTests = {
+            {.input="23", .output={"ad","ae","af","bd","be","bf","cd","ce","cf"}},
+            {.input="2", .output={"a","b","c"}},
+            {.input="", .output={}},
+    };
+    int count = 0;
+    for(auto test: vecTests){        
+        auto output = sol.letterCombinations(test.input);
+        bool bIsPass = match_array(output,test.output);
+        cout << "Test #" << ++count << " : Input = " << test.input << ", Output = " << print_array<string>(output) << ". Result : " <<  (bIsPass? green : red) << (bIsPass? "Pass" : "Fail") << "!" << def << endl;
+    }
+	return 0;
+}
+/*
+-----------------------------------------------------------------------------------------------------------
+method1: <<>>
+-----------------------------------------------------------------------------------------------------------
+Leetcode Method:
+
+-----------------------------------------------------------------------------------------------------------
+*/
+
