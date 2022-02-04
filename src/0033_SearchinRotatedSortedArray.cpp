9a676a8b7a932b3e6d89bb8dc25a4770beb6ac68
Ankit Shukla <ankitshukla34@gmail.com>
2022-02-04 10:33:23 -0500

    Problem 33 - Search in Rotated Sorted Array

--- src/0033_SearchinRotatedSortedArray.cpp
+++ src/0033_SearchinRotatedSortedArray.cpp
@@ -33,7 +33,7 @@ class Solution {
         }
         return right;
     }
-    int binary_search(vector<int>& nums, int target, int rot_offset){
+    int binary_search1(vector<int>& nums, int target, int rot_offset){
         int left = 0, right = rot_offset - 1;
         while(left <= right){
             int mid = (left + right) / 2;
@@ -57,9 +57,57 @@ class Solution {
         }
         return -1;
     }
+    int binary_search2(vector<int>& nums, int target, int rot_offset, int left, int right){
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(nums[mid] == target)
+                return mid;
+            if(target < nums[mid])
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return -1;
+    }
+
+    int singlePassSearch(vector<int>& nums, int target){
+        int left = 0, right = nums.size() - 1;
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(nums[mid] == target)
+                return mid;
+
+            if(nums[left] <= nums[mid]) {
+                if(nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            else {
+                if(nums[mid] < target && target <= nums[right])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
     int search(vector<int>& nums, int target) {
         int rotation_offset = find_rotation_offset(nums);
-        return binary_search(nums, target, rotation_offset);
+
+// Approach 1
+//        return binary_search1(nums, target, rotation_offset);
+//
+
+// Approach 2
+//        if(target < nums[0])
+//            return binary_search2(nums, target, rotation_offset, rotation_offset, nums.size() -1);
+//        else
+//            return binary_search2(nums, target, rotation_offset, 0, rotation_offset - 1);
+
+// Approach 3
+        return singlePassSearch(nums, target);
     }
 };
 int main() {
@@ -76,7 +124,7 @@ int main() {
         auto duration = duration_cast<microseconds>(high_resolution_clock::now() - start);
         bool bIsPass = output == test.output;
         time += duration.count();
-        cout << "Test #" << ++count << " : nums=" << print_array(test.nums) << ", target="<< test.target << ", Output = " << output << ", Time taken= " << duration.count()/1000.0 << " ms, Result : " <<  (bIsPass? green : red) << (bIsPass? "Pass" : "Fail") << "!" << def << endl;
+        cout << "Test #" << ++count << " : nums=" << print_array(test.nums) << ", target="<< test.target << ", Output = " << output << ", Time taken= " << duration.count() << " us, Result : " <<  (bIsPass? green : red) << (bIsPass? "Pass" : "Fail") << "!" << def << endl;
     }
     cout << "Total time taken = " << (time/1000.0) << " ms." << endl;
 	return 0;
