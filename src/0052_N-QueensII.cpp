a8332a24c1e5d098e4ddd000bb7e7b918aea8402
Ankit Shukla <ankitshukla34@gmail.com>
2022-03-25 04:45:46 -0400

    P.52

---
+++ src/0052_N-QueensII.cpp
@@ -0,0 +1,147 @@
+//LeetCode Problem 52 - N-Queens II
+/*
+-----------------------------------------------------------------------------------------------------------
+Leetcode Method:
+Approach - Backtracking:
+    1. Create a 2D vector - board with ',' as each cell value, a vector of 2D vectors - ret, set of columns, set of diagonals, set of antidiagonals
+    and pass them to backtracking method as reference
+    2. if row == size of maze, return ret
+       else
+       iterate col from 0 to size
+            If current column already exists in set of columns or current diagonal exists in set of current
+            diagonals or current anti-diagonal exists in set of anti-diagonals, continue to next interation
+            Else,
+                board[row][col] = 'Q'
+                cols.insert(col)
+                diagonals.insert(cur_diagonal), where cur_diagonal = row - col
+                antidiagonals.insert(cur_antidiagonals), where cur_diagonal = row + col
+                backtracking(row + 1, board, ret, cols, diagonals, antidiagonals)
+                board[row][col] = '.'
+                cols.erase(col)
+                diagonals.erase(cur_diagonal)
+                antidiagonals.erase(cur_antidiagonals)
+
+ Diagonals: row - col  can be used to identify diagonal elements
+
+        0   |   -1    |   -2    |   -3    |   -4    |   -5
+    -----------------------------------------------------------
+        1   |    0    |   -1    |   -2    |   -3    |   -4
+    -----------------------------------------------------------
+        2   |    1    |    0    |   -1    |   -2    |   -3
+    -----------------------------------------------------------
+        3   |    2    |    1    |    0    |   -1    |   -2
+    -----------------------------------------------------------
+        4   |    3    |    2    |    1    |    0    |   -1
+    -----------------------------------------------------------
+        5   |    4    |    3    |    2    |    1    |    0
+
+ Anti-diagonal: row + col can be used to identify antidiagonal elements
+
+        0   |    1    |    2    |    3    |    4    |    5
+    -----------------------------------------------------------
+        1   |    2    |    3    |    4    |    5    |    6
+    -----------------------------------------------------------
+        2   |    3    |    4    |    5   |     6    |    7
+    -----------------------------------------------------------
+        3   |    4    |    5   |     6    |    7    |    8
+    -----------------------------------------------------------
+        4   |    5   |     6    |    7    |    8    |    9
+    -----------------------------------------------------------
+        5   |    6    |    7    |    8    |    9    |    10
+
+ -----------------------------------------------------------------------------------------------------------
+*/
+#include "everything.h"
+
+struct Test{
+    int input;
+    int output;
+};
+class Solution {
+public:
+    void backtrack(int size, vector<vector<string>> &ret, vector<string> &board, int row, set<int> &cols, set<int> &diagonals, set<int> &antidiagonals){
+        if(row == size){
+            ret.push_back(board);
+            return;
+        }
+        for(int col = 0; col < size; col++){
+            int currDiagonal = row - col;
+            int currAntiDiagonal = row + col;
+            if(cols.find(col) != cols.end() || diagonals.find(currDiagonal) != diagonals.end() || antidiagonals.find(currAntiDiagonal) != antidiagonals.end())
+                continue;
+
+            cols.insert(col);
+            diagonals.insert(currDiagonal);
+            antidiagonals.insert(currAntiDiagonal);
+            board[row][col] = 'Q';
+
+            backtrack(size, ret, board, row + 1, cols, diagonals, antidiagonals);
+
+            cols.erase(col);
+            diagonals.erase(currDiagonal);
+            antidiagonals.erase(currAntiDiagonal);
+            board[row][col] = '.';
+        }
+    }
+    vector<vector<string>> solveNQueens1(int n) {
+        vector<vector<string>> ret;
+        vector<string> board(n, string(n, '.'));
+        set<int> cols, diagonals, antidiagonals;
+        backtrack(n, ret, board, 0, cols, diagonals, antidiagonals);
+        return ret;
+    }
+
+    int backtrack1(int size, vector<string> &board, int row, set<int> &cols, set<int> &diagonals, set<int> &antidiagonals){
+        if(row == size)
+            return 1;
+        int total = 0;
+        for(int col = 0; col < size; col++){
+            int currDiagonal = row - col;
+            int currAntiDiagonal = row + col;
+            if(cols.find(col) != cols.end() || diagonals.find(currDiagonal) != diagonals.end() || antidiagonals.find(currAntiDiagonal) != antidiagonals.end())
+                continue;
+
+            cols.insert(col);
+            diagonals.insert(currDiagonal);
+            antidiagonals.insert(currAntiDiagonal);
+            board[row][col] = 'Q';
+
+            total += backtrack1(size, board, row + 1, cols, diagonals, antidiagonals);
+
+            cols.erase(col);
+            diagonals.erase(currDiagonal);
+            antidiagonals.erase(currAntiDiagonal);
+            board[row][col] = '.';
+        }
+        return total;
+    }
+    int totalNQueens1(int n) {
+        vector<string> board(n, string(n, '.'));
+        set<int> cols, diagonals, antidiagonals;
+        return backtrack1(n, board, 0, cols, diagonals, antidiagonals);
+    }
+
+    int totalNQueens(int n) {
+        //return solveNQueens1(n).size();
+        return totalNQueens1(n);
+    }
+};
+int main() {
+    Solution sol;
+    vector<Test> vecTests = {
+            {4, 2},
+            {1, 1}
+    };
+    int count = 0, time = 0;
+    for(auto test: vecTests){
+        auto start = high_resolution_clock::now();
+        auto output = sol.totalNQueens(test.input);
+        auto duration = duration_cast<microseconds>(high_resolution_clock::now() - start);
+        bool bIsPass = output == test.output;
+        time += duration.count();
+        cout << "Test #" << ++count << " : Input = " << test.input << ", Output = " << output << ", Time taken= " << duration.count()/1000.0 << " ms, Result : " <<  (bIsPass? green : red) << (bIsPass? "Pass" : "Fail") << "!" << def << endl;
+    }
+    cout << "Total time taken = " << (time/1000.0) << " ms." << endl;
+    return 0;
+}
+
